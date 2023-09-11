92a1c69eb6270cbeacd9bccd2a2d0945786569c7
AM Codes <amccodes@gmail.com>
2023-09-11 22:51:56 +0700

    delete all unused image

--- .next/server/flight-server-css-manifest.js
+++ .next/server/flight-server-css-manifest.js
@@ -1,18 +1,1 @@
-self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/Users/macos/React/adimurianto/src/app/layout.tsx": [
-      "/Users/macos/React/adimurianto/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
-    ],
-    "/Users/macos/React/adimurianto/src/app/page.tsx": [
-      "/Users/macos/React/adimurianto/node_modules/react-vertical-timeline-component/style.min.css",
-      "/Users/macos/React/adimurianto/src/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/Users/macos/React/adimurianto/src/app/page": [
-      "/Users/macos/React/adimurianto/src/app/globals.css",
-      "/Users/macos/React/adimurianto/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
-      "/Users/macos/React/adimurianto/node_modules/react-vertical-timeline-component/style.min.css"
-    ]
-  }
-}
+self.__RSC_CSS_MANIFEST={"cssImports":{"/Users/macos/React/adimurianto/src/app/page.tsx":["/Users/macos/React/adimurianto/src/app/globals.css","/Users/macos/React/adimurianto/node_modules/react-vertical-timeline-component/style.min.css"],"/Users/macos/React/adimurianto/src/app/layout.tsx":["/Users/macos/React/adimurianto/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"]},"cssModules":{"/Users/macos/React/adimurianto/src/app/page":["/Users/macos/React/adimurianto/src/app/globals.css","/Users/macos/React/adimurianto/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}","/Users/macos/React/adimurianto/node_modules/react-vertical-timeline-component/style.min.css"]}}
